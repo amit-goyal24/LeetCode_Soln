3fd570cd84aaedae893e7f17fca4f2048aab4a4a
Amit Goyal <amitpvt0150@gmail.com>
2026-07-21 09:17:06 +0530

    Time: 22 ms (1.28%), Space: 43.8 MB (12.15%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -4,12 +4,12 @@ class Solution {
     
     // Optimised Soln
 
-    int count =0;
-    while(n>0){
-        count++;
-        n=(n&(n-1));
-    }
-    return count;
+    // int count =0;
+    // while(n>0){
+    //     count++;
+    //     n=(n&(n-1));
+    // }
+    // return count;
 
     //  String bin = Integer.toBinaryString(n); 
     //  int count = 0;
@@ -17,9 +17,21 @@ class Solution {
     //     if(ch=='1')count++;
     //  }  
     //  return count;
-    }
-}
 
+
+    //bit manupulation
+
+    int count =0;
+    for(int i=0;i<32;i++){
+        if(((n>>i)&1)==1){
+            System.out.println(n+" " +i);
+            count++;
+        }
+        }return count;
+    }
+
+
+    }
 
 
 //return Integer bitcount(n);  method 2 
